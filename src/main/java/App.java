6d5b0a11903b761d0e02abbb4da6a6620e3243ca
Flugeggehaimen <bliznyuk10@mail.ru>
2022-07-18 18:35:47 +0300

    task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Сравнение bean helloworld по ссылке - " + (bean == bean1));
+
+        Cat beanCatOne = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCatOne.getMessage());
+        Cat beanCatTwo = (Cat) applicationContext.getBean("cat");
+        System.out.println("Сравнение bean cat по ссылке - " + (beanCatOne == beanCatTwo));
+
     }
 }
